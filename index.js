51da204db94fff562279bdd1bad22bb0c7f7eb68
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-10-02 22:44:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Interação javascript utilizando JQuery
+
+$(document).ready(function () {
+  const navbar = $(".navbar");
+  const mobileNavbar = $(".navbar__mobile");
+  const button = $(".burguer");
+
+  // Adicionando evento de click no botão menu hambúrguer na navbar do modo mobile
+  button.on("click", function () {
+    mobileNavbar.toggleClass("active");
+  });
+
+  // Adicionando evento de scroll na navbar para aplicar o sombreamento quando a distô do scrollTop for > 0
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 0) {
+      navbar.addClass("active");
+    } else {
+      navbar.removeClass("active");
+    }
+  });
+});
